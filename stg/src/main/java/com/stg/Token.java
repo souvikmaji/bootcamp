953e72a59d1038faefe94c38f429957bf345fc09
souvikmaji <souvikmaji94@gmail.com>
2019-05-18 20:00:07 +0530

    token generator implemented

--- stg/src/main/java/com/stg/Token.java
+++ stg/src/main/java/com/stg/Token.java
@@ -1,9 +1,26 @@
 package com.stg;
 
+import java.security.SecureRandom;
+
 public class Token {
-	private String token;
-	
-	public Token(int len) {
-		
+	private static final String symbols = "abcdefghijklmnopqrstuvwxyz0123456789";
+	private final char[] buf;
+	private final SecureRandom random;
+
+	public Token(int length) {
+		if (length < 1) {
+			throw new IllegalArgumentException("length < 1: " + length);
+		}
+
+		this.random = new SecureRandom();
+		this.buf = new char[length];
+	}
+
+	public String generate() {
+		for (int idx = 0; idx < buf.length; ++idx) {
+			buf[idx] = symbols.charAt(random.nextInt(symbols.length()));
+		}
+			
+		return new String(buf);
 	}
 }
